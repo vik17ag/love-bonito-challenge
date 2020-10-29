7a756141d352266177515d31c54bc63eaa994cc1
vik17ag <vik17ag@gmail.com>
2020-10-29 19:15:17 +0530

    style: Make character card responsive

--- src/views/Character.vue
+++ src/views/Character.vue
@@ -2,21 +2,35 @@
       <div id="character" >
           <button @click="$router.back()">Back</button>
 
-          <b-card
+          <!--<b-card
                   :img-alt="character.name"
-                  img-start
+                  img-top
                  :img-src="character.image">
 
-              <b-card-title :title="character.name"></b-card-title>
-              <b-card-text>
-                  <b-badge :variant="getStatusColor(character.status)">{{character.status}}</b-badge>
-              </b-card-text>
-              <b-card-sub-title>{{character.species}} | <a :class="getGenderColor(character.gender)">{{character.gender}}</a></b-card-sub-title>
-              <b-card-text>Birth date : {{new Date(character.created).toDateString()}}</b-card-text>
-              <b-card-text>Last Known location : {{character.location.name}}</b-card-text>
-              <b-card-text>First seen in : {{character.origin.name}}</b-card-text>
 
-          </b-card>
+
+          </b-card>-->
+
+          <div align="center">
+              <b-card  no-body class="overflow-hidden" style="max-width: 540px;">
+                  <b-row no-gutters>
+                      <b-col md="6">
+                          <b-card-img :src="character.image" :alt="character.name" class="rounded-0"></b-card-img>
+                      </b-col>
+                      <b-col md="6">
+                          <b-card-body :title="character.name">
+                              <b-card-text>
+                                  <b-badge :variant="getStatusColor(character.status)">{{character.status}}</b-badge>
+                              </b-card-text>
+                              <b-card-sub-title>{{character.species}} | <a :class="getGenderColor(character.gender)">{{character.gender}}</a></b-card-sub-title>
+                              <b-card-text>Birth date : {{new Date(character.created).toDateString()}}</b-card-text>
+                              <b-card-text>Last Known location : {{character.location.name}}</b-card-text>
+                              <b-card-text>First seen in : {{character.origin.name}}</b-card-text>
+                          </b-card-body>
+                      </b-col>
+                  </b-row>
+              </b-card>
+          </div>
 
           <h3>Appeared in</h3>
               <b-card no-gutters class="mb-2 " text-variant="primary" v-for="(ep,index) in character.episode" v-bind:key="index">Episode {{index+1}}</b-card>
@@ -30,7 +44,9 @@
 
     import { BBadge } from 'bootstrap-vue'
     Vue.component('b-badge', BBadge)
-    import { CardPlugin } from 'bootstrap-vue'
+    Vue.component('b-row', BRow)
+    Vue.component('b-col', BCol)
+    import { CardPlugin, BRow, BCol } from 'bootstrap-vue'
     Vue.use(CardPlugin)
     import 'bootstrap/dist/css/bootstrap.css'
     import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -43,7 +59,8 @@
             return {
                 character: {
                     location : {},
-                    origin : {}
+                    origin : {},
+                    image: ''
                 }
             }
         },
