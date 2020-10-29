388ed6554595df0cfc4ffec5a704dd010a98baf6
vik17ag <vik17ag@gmail.com>
2020-10-29 14:24:23 +0530

    feat: Add Dexie to store in IndexedDb

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -1,50 +1,51 @@
 <template>
-  <div id="characters">
-      <button  @click="$emit('openLocation','Locations')">Back</button>
-      <h3>Characters</h3>
+    <div id="characters">
+        <button  @click="$emit('openLocation','Locations')">Back</button>
+        <h3>Characters</h3>
 
-      <b-card  @click="$router.push({ name: 'Character', params: { id: character.id } })"
-              img-alt="Image"
-              img-top
-              style="max-width: 20rem;"
-              class="mb-2" :img-src="character.image" v-for="(character,index) in characters" v-bind:key="index">
+        <b-card  @click="$router.push({ name: 'Character', params: { id: character.id } })"
+                 img-alt="Image"
+                 img-top
+                 style="max-width: 20rem;"
+                 class="mb-2" :img-src="character.image" v-for="(character,index) in characters" v-bind:key="index">
 
-          <b-card-title :title="character.name"></b-card-title>
-          <b-card-text>
-            <b-badge :variant="getStatusColor(character.status)">{{character.status}}</b-badge>
-          </b-card-text>
-          <b-card-sub-title>{{character.species}} | <a :class="getGenderColor(character.gender)">{{character.gender}}</a></b-card-sub-title>
-      </b-card>
-  </div>
+            <b-card-title :title="character.name"></b-card-title>
+            <b-card-text>
+                <b-badge :variant="getStatusColor(character.status)">{{character.status}}</b-badge>
+            </b-card-text>
+            <b-card-sub-title>{{character.species}} | <a :class="getGenderColor(character.gender)">{{character.gender}}</a></b-card-sub-title>
+        </b-card>
+    </div>
 </template>
 
 <script>
     import axios from 'axios'
     import Vue from 'vue'
     import { BBadge } from 'bootstrap-vue'
+    import Dexie from 'dexie'
     Vue.component('b-badge', BBadge)
 
     export default {
-  name: 'Characters',
-  props: {
-      ids: String
-  },
-  data(){
-      return {
-          characters: []
-      }
-  },
+        name: 'Characters',
+        props: {
+            ids: String
+        },
+        data(){
+            return {
+                characters: []
+            }
+        },
         methods:{
             getStatusColor(status){
-          switch (status){
-              case 'Dead':
-                  return 'danger'
-              case 'Alive':
-                  return 'success'
-               default:
-                  return 'secondary'
-          }
-      },
+                switch (status){
+                    case 'Dead':
+                        return 'danger'
+                    case 'Alive':
+                        return 'success'
+                    default:
+                        return 'secondary'
+                }
+            },
             getGenderColor(gender){
                 switch (gender){
                     case 'Male':
@@ -56,15 +57,52 @@
                 }
             }
         },
-    mounted(){
-        axios
-            .get('https://rickandmortyapi.com/api/character/'+this.ids)
-            .then(response => {
-                //console.log(response.data)
-                this.characters = Array.isArray(response.data)?response.data:[response.data]
-            })
+        async mounted(){
+            this.$emit('error',null)
+
+            this.db = new Dexie("Bonito");
+            this.db.version(5).stores({
+                locations: 'id,page',
+                characters: 'id, location'
+            });
+
+            //Fetch from indexedDb
+            this.characters = await this.db.characters.where('location').equals('https://rickandmortyapi.com/api/character/'+this.ids).toArray()
+
+                .catch((error) => {
+                    console.error("Failed to fetch characters from indexedDb. Error: " + error);
+                    this.$emit('error',3)
+                })
+
+            //If no data available, then fetch from api
+            if (!this.characters || this.characters.length === 0){
+
+                axios
+                    .get('https://rickandmortyapi.com/api/character/'+this.ids)
+                    .then(response => {
+                        this.characters = Array.isArray(response.data)?response.data:[response.data]
+
+                        //Save in db along with location id as a direct attribute
+                        this.db.characters.bulkPut(this.characters.map(char => {
+                            return {...char, location: char.location.url}
+                        }))
+                            .catch((error) => {
+                                console.error("Failed to add characters in indexedDb. Error: " + error);
+                                this.$emit('error',3)
+                            })
+
+                    })
+
+                    .catch((error) => {
+                        console.error("Failed to fetch from api. Error: " + error);
+                        this.$emit('error',1)
+                    })
+
+            }
+
+
+        }
     }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/components/Locations.vue
+++ src/components/Locations.vue
@@ -1,84 +1,133 @@
 <template>
-  <div id="locations">
-    <h3>Locations</h3>
-      <b-pagination
-              id="pagination"
-              :value="page"
-              :total-rows="rows"
+    <div id="locations">
+        <h3>Locations</h3>
+
+        <b-pagination
+                id="pagination"
+                :value="page"
+                :total-rows="rows"
               :per-page="perPage"
-              @change="handlePageChange"
-      />
-      <b-card :id="'card' + index" v-for="(location,index) in locations" v-bind:key="index">
-          <b-card-title :title="location.name"></b-card-title>
-          <b-card-sub-title >{{location.dimension}}</b-card-sub-title>
-          <b-card-text >{{location.type}}</b-card-text>
-          <button @click="openCharacter(location.residents)">View Characters</button>
-      </b-card>
-  </div>
+                @change="handlePageChange"
+        />
+        <b-card :id="'card' + index" v-for="(location,index) in locations" v-bind:key="index">
+            <b-card-title :title="location.name"></b-card-title>
+            <b-card-sub-title >{{location.dimension}}</b-card-sub-title>
+            <b-card-text >{{location.type}}</b-card-text>
+            <button @click="openCharacter(location.residents)">View Characters</button>
+        </b-card>
+    </div>
 </template>
 
 <script>
     import axios from 'axios'
     import Vue from 'vue'
+    import Dexie from "dexie"
+
     import { BPagination } from 'bootstrap-vue'
     Vue.component('b-pagination', BPagination)
 
-export default {
-  name: 'Locations',
-  props: {
-  },
-    data(){
-      return {
-          locations: [],
-          perPage: 0
-
-      }
-    },
-    computed : {
-        rows:{
-          get(){
-              return this.$store.state.rows
-          },
-          set(value){
-              this.$store.commit('updateLocationCount',value)
-          }
-      },
-        page:{
-            get(){
-                return this.$store.state.page
-            },
-            set(value){
-                this.$store.commit('updatePage',value)
-            }
-        }
-    },
-    methods:{
-      getLocations(){
-          axios
-              .get('https://rickandmortyapi.com/api/location?page='+this.page)
-              .then(response => {
-                  //console.log(response)
-                  this.locations = response.data.results
-                  this.rows = response.data.info.count
-              })
-      },
-        handlePageChange(value) {
-           this.page = value
-            this.getLocations()
+    export default {
+        name: 'Locations',
+        props: {
         },
-        openCharacter(residents){
-          if (residents &&  residents.length > 0){
-              this.$emit('openCharacter',residents.map(url=>url.split('/')[5]).toString())
-          } else {
-              alert('No one is residing')
-          }
-        }
-    },
-    mounted(){
-        this.getLocations()
+        data(){
+            return {
+                locations: [],
+                perPage: 0,
+                db: {}
+            }
+        },
+        computed : {
+            rows:{
+                get(){
+                    return this.$store.state.rows
+                },
+                set(value){
+                    this.$store.commit('updateLocationCount',value)
+                }
+            },
+            page:{
+                get(){
+                    return this.$store.state.page
+                },
+                set(value){
+                    this.$store.commit('updatePage',value)
+                }
+            }
+        },
+        methods:{
+            async  getLocations(){
+                this.$emit('error',null)
 
+
+                //Fetch from indexedDb
+                this.locations = await this.db.locations.where('page').equals(this.page).toArray()
+                    .catch((error) => {
+                        console.error("Failed to fetch locations from indexedDb. Error: " + error);
+                        this.$emit('error',3)
+                    })
+                this.rows = localStorage.getItem('rows')
+
+                //If no data available, then call api
+                if (this.locations.length === 0 || this.rows) {
+
+                    let response = await axios.get('https://rickandmortyapi.com/api/location?page=' + this.page)
+
+                        .catch((error) => {
+                            console.error("Failed to fetch from api. Error: " + error);
+                            if (error.request){
+                                this.$emit('error',1)
+                                //: 404 Not Found page/error message if your API returns a 404.
+                                //: 5xx Backend error message
+                                //: retry for spotty network or show refresh page - axios-retry
+
+                            } else {
+                                this.$emit('error',4)
+                            }
+                        })
+                    this.locations = response.data.results
+                    this.locations.page = this.page
+                    this.rows = response.data.info.count
+
+                    localStorage.setItem('rows', response.data.info.count)
+
+                    //Save in db along with page attribute
+                    this.db.locations.bulkPut(response.data.results.map(loc => {
+                        return {...loc, page: this.page}
+                    }))
+                        .catch((error) => {
+                            console.error("Failed to add locations in indexedDb. Error: " + error)
+                            this.$emit('error',3)
+
+
+                        })
+
+                }
+
+            },
+            handlePageChange(value) {
+                this.page = value
+                this.getLocations()
+            },
+            openCharacter(residents){
+                if (residents &&  residents.length > 0){
+                    this.$emit('openCharacter',residents.map(url=>url.split('/')[5]).toString())
+                } else {
+                    alert('No one is residing')
+                }
+            }
+        },
+        mounted(){
+            this.db = new Dexie("Bonito");
+            this.db.version(5).stores({
+                locations: 'id,page',
+                characters: 'id, location'
+            });
+
+            this.getLocations()
+
+        }
     }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
